e89fe52f9cd4f7faffcff2cd15fc87369620919e
supanutsingson42-droid <supanutsingson42@gmail.com>
2026-03-18 15:45:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDBbcVo3VZcCgosegExwU72h0s2RTmjBI8",
+  authDomain: "lucky-spin-155a5.firebaseapp.com",
+  projectId: "lucky-spin-155a5",
+  storageBucket: "lucky-spin-155a5.firebasestorage.app",
+  messagingSenderId: "416059719807",
+  appId: "1:416059719807:web:923f80e9a060295c94bf4a"
+};
